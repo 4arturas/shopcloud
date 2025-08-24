1f38e5c8648f94fefaef239671017e5febbe27a7
Your Name <you@example.com>
2025-08-24 11:52:24 +0300

    2025-08-24

--- shopcloud-infra/modules/kubernetes-base/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "namespace_name" {
-  description = "The name of the Kubernetes namespace."
-  value       = kubernetes_namespace.test.metadata[0].name
-}
--- shopcloud-infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "kafka_network_name" {
-  description = "The name of the Docker network created for Kafka."
-  value       = module.kafka.kafka_network_name
-}
-
-output "kubernetes_namespace" {
-  description = "The name of the Kubernetes namespace."
-  value       = module.kubernetes_base.namespace_name
-}
