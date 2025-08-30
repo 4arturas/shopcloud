1a2c6d24b9d385d3251ed4e7f7a2f661b8512b60
Your Name <you@example.com>
2025-08-30 12:46:19 +0300

    2025-08-30

---
+++ cloud/cloud-api-gateway/src/main/java/com/example/apigateway/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.example.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API Gateway").version("1.0").description("Documentation for API Gateway"));
+    }
+}
