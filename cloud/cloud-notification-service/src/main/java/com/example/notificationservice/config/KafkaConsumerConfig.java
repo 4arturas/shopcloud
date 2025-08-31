64562fbe57e9cbcab486fd9db14754efdcb9432b
Your Name <you@example.com>
2025-08-31 09:22:21 +0300

    2025-08-31

--- cloud/cloud-notification-service/src/main/java/com/example/notificationservice/config/KafkaConsumerConfig.java
+++ cloud/cloud-notification-service/src/main/java/com/example/notificationservice/config/KafkaConsumerConfig.java
@@ -2,8 +2,10 @@ package com.example.notificationservice.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
@@ -16,10 +18,13 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("spring.kafka.bootstrap-servers"));
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
