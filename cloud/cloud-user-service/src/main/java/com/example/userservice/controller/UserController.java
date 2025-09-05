9af7cb204ae17ab4b4b923ff4473cd62f6558325
Your Name <you@example.com>
2025-09-05 19:23:03 +0300

    2025-09-05

--- cloud/cloud-user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ cloud/cloud-user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,10 +1,5 @@
 package com.example.userservice.controller;
 
-import com.example.userservice.entity.User;
-import com.example.userservice.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +7,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
-
-    @PostMapping("/register")
-    public User register(@RequestBody User user) {
-        return userService.register(user);
-    }
 }
--- cloud/cloud-user-service/src/main/java/com/example/userservice/service/UserService.java
+++ cloud/cloud-user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -1,32 +1,13 @@
 package com.example.userservice.service;
 
-import com.example.userservice.entity.User;
-import com.example.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    public User register(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        User savedUser = userRepository.save(user);
-        kafkaTemplate.send("user-registered", savedUser.getUsername());
-        return savedUser;
-    }
-
-    public User findByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
 }
